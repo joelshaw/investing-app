353f6885e5b7e6d099a6bc278e674c306354b1b5
joelshaw <jshaw@rvadesignsolutions.com>
2020-03-03 20:20:47 -0500

    Generated accounts, clients, and positions components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { ClientsComponent } from './clients/clients.component';
+import { PositionsComponent } from './positions/positions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountsComponent,
+    ClientsComponent,
+    PositionsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/positions/positions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-positions',
+  templateUrl: './positions.component.html',
+  styleUrls: ['./positions.component.less']
+})
+export class PositionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
